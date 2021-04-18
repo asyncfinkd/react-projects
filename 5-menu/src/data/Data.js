5989f6325aa3900e3de6f961c6f1cbea24c9a0a0
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-18 19:58:19 +0400

    Create Data.js

---
+++ 5-menu/src/data/Data.js
@@ -0,0 +1,24 @@
+const menu = [
+  {
+    id: 1,
+    title: "iphone 12",
+    category: "phone",
+    price: "4999.99",
+    img:
+      "https://cdn.tmobile.com/content/dam/t-mobile/en-p/cell-phones/apple/Apple-iPhone-12-Pro/Pacific-Blue/Apple-iPhone-12-Pro-Pacific-Blue-frontimage.png",
+    desc:
+      "The iPhone 12 and iPhone 12 mini are Apple's mainstream flagship iPhones for 2020. The phones come in 6.1-inch and 5.4-inch sizes with identical features",
+  },
+  {
+    id: 2,
+    title: "Samsung S21",
+    category: "phone",
+    price: "3999.99",
+    img:
+      "https://images.samsung.com/is/image/samsung/p6pim/levant/sm-g991bzadmea/gallery/levant-galaxy-s21-5g-g991-sm-g991bzadmea-388257277?$720_576_PNG$",
+    desc:
+      "Click on any of the prices to see the best deals from the corresponding store. We may get a commission from qualifying sales.",
+  },
+];
+
+export default menu;
