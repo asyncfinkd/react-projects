607e70401504646619c3c17c5a9a7511e24e3981
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-18 19:14:29 +0400

    Create Data.js

---
+++ 4-accordion/src/data/Data.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    id: 1,
+    question: "Who is Facebook CEO?",
+    answer: "Mark Zuckerberg is facebook CEO",
+  },
+  {
+    id: 2,
+    question: "Who is Google CEO?",
+    answer: "Sundar Pichai is google CEO",
+  },
+  {
+    id: 3,
+    question: "Who is Microsoft CO-CEO?",
+    answer: "Bill Gates is microsoft CO-CEO",
+  },
+];
