99d46e5afb71077dee6f0bad60e0be037154faba
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-18 12:07:41 +0400

    Push

--- 1-birthday-reminder/src/App.js
+++ 1-birthday-reminder/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
+import List from "./list/List";
 
 export default function App() {
   return (
     <>
-      <p>i</p>
+      <main>
+        <section class="container">
+          <h3>0 birthdays today</h3>
+          <List />
+        </section>
+      </main>
     </>
   );
 }
