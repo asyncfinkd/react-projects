5cd3ab7f14e76fc0ad973254a6047e5147df0d6e
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-18 11:57:21 +0400

    Full Birthday Data list

--- 1-birthday-reminder/src/data/Data.js
+++ 1-birthday-reminder/src/data/Data.js
@@ -1,5 +1,30 @@
 export const Birthday = [
   {
+    id: "0",
     username: "Bill Gates",
+    image:
+      "https://www.cnet.com/a/img/L9-S0VZL7Q6SI_slQ7pFeZMUm34=/940x0/2018/03/15/22b11eba-6122-4537-8dd1-a623d8fc1d90/iosgz49p-400x400.jpg",
+    age: "65",
+  },
+  {
+    id: "1",
+    username: "Mark Zuckerberg",
+    image:
+      "https://thumbor.forbes.com/thumbor/400x400/http://specials-images.forbes.com/imageserve/09kZdVWgJO0tu/960x960.jpg?fit=scale&background=000000",
+    age: "36",
+  },
+  {
+    id: "2",
+    username: "Elon Musk",
+    image:
+      "https://images.indulgexpress.com/uploads/user/imagelibrary/2020/10/7/original/ElonMusk.jpg",
+    age: "49",
+  },
+  {
+    id: "3",
+    username: "Jeff Bezos",
+    image:
+      "https://global-uploads.webflow.com/5dfd5aca7badfa129f80056c/5f89c9db933d223a087f5aa3_JeffBezosRecommendedBooks.jpeg",
+    age: "57",
   },
 ];
