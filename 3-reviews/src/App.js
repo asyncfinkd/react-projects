71555921dfac16adb9003a72576320ae229012af
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-18 14:06:36 +0400

    Add React Icons (in project)

--- 3-reviews/src/App.js
+++ 3-reviews/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Review from "./review/Review";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function App() {
   return (
     <>
-      <p>Hello</p>
+      <h2>
+        reviews project setup <FaGithubSquare />
+      </h2>
     </>
   );
 }
