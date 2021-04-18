a9df5ca27f18cec609eb1ace12a937fb2440b68d
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-18 14:33:13 +0400

    Functionallity Prev, Next

--- 3-reviews/src/review/Review.js
+++ 3-reviews/src/review/Review.js
@@ -5,6 +5,32 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 export default function Review() {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      const zero = 0;
+      return zero;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
   return (
     <>
       <article className="review">
@@ -18,10 +44,10 @@ export default function Review() {
         <p className="job">{job}</p>
         <p className="info">{text}</p>
         <div className="button-container">
-          <button className="prev-btn">
+          <button className="prev-btn" onClick={prevPerson}>
             <FaChevronLeft />
           </button>
-          <button className="next-btn">
+          <button className="next-btn" onClick={nextPerson}>
             <FaChevronRight />
           </button>
         </div>
