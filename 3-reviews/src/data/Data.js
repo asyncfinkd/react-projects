b13d717cea862ead915f721d184ba716b1f2a2a8
asyncfinkd <shamgoingzuck@gmail.com>
2021-04-18 14:20:30 +0400

    Design, Not functionallity

--- 3-reviews/src/data/Data.js
+++ 3-reviews/src/data/Data.js
@@ -6,7 +6,7 @@ const reviews = [
     image:
       "https://thumbor.forbes.com/thumbor/400x400/http://specials-images.forbes.com/imageserve/09kZdVWgJO0tu/960x960.jpg?fit=scale&background=000000",
     text:
-      "I'm just writing to thank you for this product. I had failed in so many interviews before, but I wanted to get into a top tech company so much that I even enrolled in a Master's program. Even then, I was unsure if I had what it takes to make it. From the moment I heard your first video explanation, I thought 'this is exactly the way to solve an interview question' (plus the extra points you can grab by asking clarifying questions). After a few months of studying, mainly on AlgoExpert, I got offers to intern at Microsoft and Google!",
+      "I'm just writing to thank you for this product. I had failed in so many interviews before, but I wanted to get into a top tech company so much that I even enrolled in a Master's program. Even then, I was unsure if I had what it takes to make it. From the moment I heard your first video explanation.",
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ const reviews = [
     image:
       "https://www.cnet.com/a/img/L9-S0VZL7Q6SI_slQ7pFeZMUm34=/940x0/2018/03/15/22b11eba-6122-4537-8dd1-a623d8fc1d90/iosgz49p-400x400.jpg",
     text:
-      "AlgoExpert was the backbone of my technical coding interview preparation. It allows you to efficiently work through the most common variations of problems asked by top-tier companies without having to spend hours 'battling' an algorithm only to come up with an inefficient or incorrect solution. There are a lot of resources available for repetition, but AlgoExpert differentiates its product by providing the 'how' and 'why' in clear and concise videos. Developing a deeper understanding of how to approach these problems is better than trying to memorize lines of code. I highly recommend AlgoExpert.",
+      "AlgoExpert was the backbone of my technical coding interview preparation. It allows you to efficiently work through the most common variations of problems asked by top-tier companies without having to spend hours 'battling' an algorithm only to come up with an inefficient or incorrect solution.",
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ const reviews = [
     image:
       "https://global-uploads.webflow.com/5dfd5aca7badfa129f80056c/5f89c9db933d223a087f5aa3_JeffBezosRecommendedBooks.jpeg",
     text:
-      "As a scientist who was looking to break into Tech, I knew the underlying logic of programming, but I had a lot of gaps in my understanding, especially on the types of algorithms questions asked at interviews. I can confidently say that AlgoExpert is one of the best resources out there for interview preparation, with fantastic video tutorials and an excellent question selection that allows you to get a deep understanding of the topics and confidence in your problem solving ability. The site is incredibly intuitive to use and I think that the staff are some of the best out there, being incredibly supportive and passionate about offering a great customer experience. I cannot recommend AlgoExpert highly enough.",
+      "As a scientist who was looking to break into Tech, I knew the underlying logic of programming, but I had a lot of gaps in my understanding, especially on the types of algorithms questions asked at interviews. I can confidently say that AlgoExpert is one of the best resources out there for interview preparation",
   },
 ];
 
--- 3-reviews/src/review/Review.js
+++ 3-reviews/src/review/Review.js
@@ -4,9 +4,29 @@ import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 export default function Review() {
   const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
   return (
     <>
-      <h2>Review</h2>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="button-container">
+          <button className="prev-btn">
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn">
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className="random-btn">Surprise me</button>
+      </article>
     </>
   );
 }
